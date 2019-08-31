44c3b037a71d8aaefe83f373eabd27705b0b5b31
kirill50 <kirill50@list.ru>
2019-08-31 22:01:36 +0300

    Внес правки по дз

--- hometask 3,4.sql
+++ hometask 3,4.sql
@@ -3,13 +3,15 @@ Hometask 3
 1. Пусть в таблице users поля created_at и updated_at оказались незаполненными. 
 Заполните их текущими датой и временем.
 
-UPDATE customers SET created_at = NOW() WHERE id > 0;
-UPDATE customers SET updateded_at = NOW() WHERE id > 0;
+UPDATE customers SET created_at = NOW();
+UPDATE customers SET updateded_at = NOW();
 2. Таблица users была неудачно спроектирована. Записи created_at и updated_at были заданытипом VARCHAR и в них долгое время помещались значения в формате "20.10.2017 8:10".Необходимо преобразовать поля к типу DATETIME, сохранив введеные ранее значения.
 
-UPDATE customers SET created_at=STR_TO_DATE(updated_at,'%d.%m.%Y %H:%i');
+UPDATE customers SET created_at=STR_TO_DATE(created_at,'%d.%m.%Y %H:%i');
 UPDATE customers SET updated_at=STR_TO_DATE(updated_at,'%d.%m.%Y %H:%i');
 
+ALTER TABLE customers ALTER COLUMN created_at DATETIME 
+ALTER TABLE customers ALTER COLUMN updated_at DATETIME
 3. В таблице складских запасов storehouses_products в поле value могут встречаться самыеразные цифры: 0, если товар закончился и выше нуля, если на складе имеются запасы.Необходимо отсортировать записи таким образом, чтобы они выводились в порядкеувеличения значения value. Однако, нулевые запасы должны выводиться в конце, после всехзаписей.
 
 SELECT id, value FROM ( 
